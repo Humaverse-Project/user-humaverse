8ef7239b1f97d9f5c09439c08232725af6369fe4
Félin Aldric <ninja.recherch@gmail.com>
2023-08-01 11:15:46 +0300

    [feat] ajout de maquette dasboard

--- src/features/header/HeaderGlobal.jsx
+++ src/features/header/HeaderGlobal.jsx
@@ -13,7 +13,7 @@ function HeaderGlobal() {
 
     return (
         <Row justifyContent={'space-between'} px={3} height={'10vh'}>
-            <DynamicHeadNav title={'ACCUEIL'} />
+            <DynamicHeadNav title={'Dashboard'} />
             {!matcheMD && (
                 <Text variant="bigTitleBold" color="blue.main" mr={20}>
                     YUMA
--- src/features/home/components/HomeScreen.jsx
+++ src/features/home/components/HomeScreen.jsx
@@ -4,18 +4,43 @@ import { useTheme } from '@mui/material/styles'
 import { Box } from '@mui/system'
 import { Fragment } from 'react'
 import { Card } from '@mui/material'
-import { NavLink } from 'react-router-dom'
-import { CardItem } from './CardNavigation'
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material'
+import { BarPlot } from '@mui/x-charts/BarChart';
+import { LinePlot } from '@mui/x-charts/LineChart';
+import { ChartContainer } from '@mui/x-charts/ChartContainer';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
 
 //ICONES
-import BuildOutlinedIcon from '@mui/icons-material/BuildOutlined'
-import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined'
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined'
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 
 function HomeScreen() {
     const theme = useTheme()
-
+    const series = [
+        {
+          type: 'bar',
+          stack: '',
+          yAxisKey: 'eco',
+          data: [2, 5, 3, 4, 1, 5, 6],
+        }
+    ];
+    const seriesformation = [
+        {
+          type: 'bar',
+          stack: '',
+          yAxisKey: 'eco',
+          data: [2, 5, 3, 4, 1, 5, 6],
+        },
+        {
+            type: 'bar',
+            stack: '',
+            yAxisKey: 'eco',
+            data: [1, 56, 2, 45, 4, 7, 2],
+        }
+    ];
     return (
         <Fragment>
             <HeaderGlobal />
@@ -25,7 +50,7 @@ function HomeScreen() {
                 flexDirection="column"
                 justifyContent="center"
                 alignItems="center"
-                height={'88vh'}
+                height={'auto'}
             >
                 <Box
                     sx={{
@@ -38,11 +63,7 @@ function HomeScreen() {
                         alignItems: 'center',
                     }}
                 >
-                    <NavLink to={'/organigramme'}>
-                        <Card
-                            sx={{
-                                backgroundColor: 'secondary.dark',
-                                m: 2,
+                    <Card sx={{ display: 'flex', width: 500, m: 2,
                                 [theme.breakpoints.up('lg')]: {
                                     m: 4,
                                 },
@@ -51,17 +72,32 @@ function HomeScreen() {
                                     width: '100%',
                                     my: 1,
                                     mx: 0,
-                                },
-                            }}
-                        >
-                            {CardItem(GroupsOutlinedIcon)}
-                        </Card>
-                    </NavLink>
-                    <NavLink to={'/test'}>
-                        <Card
-                            sx={{
-                                backgroundColor: 'secondary.dark',
-                                m: 2,
+                                }
+                            }}>
+                        <CardActionArea sx={{ width: 150 }}>
+                            <CardContent sx={{ p: 4 }}>
+                                <GroupsOutlinedIcon sx={{ fontSize: 92, color: 'black.main' }} />
+                            </CardContent>
+                        </CardActionArea>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    Nombre d’utilisateurs RH
+                                </Typography>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        TOTAL : 
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        Nouveaux : 
+                                    </Typography>
+                                </Box>
+                            </CardContent>
+                        </Box>
+                    </Card>
+                    <Card sx={{ display: 'flex', width: 500, m: 2,
                                 [theme.breakpoints.up('lg')]: {
                                     m: 4,
                                 },
@@ -70,14 +106,31 @@ function HomeScreen() {
                                     width: '100%',
                                     my: 1,
                                     mx: 0,
-                                },
-                            }}
-                        >
-                            {CardItem(BuildOutlinedIcon)}
-                        </Card>
-                    </NavLink>
+                                } }}>
+                        <CardActionArea sx={{ width: 150 }}>
+                            <CardContent sx={{ p: 4 }}>
+                                <LibraryBooksIcon sx={{ fontSize: 92, color: 'black.main' }} />
+                            </CardContent>
+                        </CardActionArea>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    Nombre de formations et test
+                                </Typography>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        Disponnible : 
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        Acheté : 
+                                    </Typography>
+                                </Box>
+                            </CardContent>
+                        </Box>
+                    </Card>
                 </Box>
-
                 <Box
                     sx={{
                         display: 'flex',
@@ -89,11 +142,7 @@ function HomeScreen() {
                         alignItems: 'center',
                     }}
                 >
-                    <NavLink to={'/test'}>
-                        <Card
-                            sx={{
-                                backgroundColor: 'secondary.dark',
-                                m: 2,
+                    <Card sx={{ display: 'flex', width: 500, m: 2,
                                 [theme.breakpoints.up('lg')]: {
                                     m: 4,
                                 },
@@ -102,17 +151,44 @@ function HomeScreen() {
                                     width: '100%',
                                     my: 1,
                                     mx: 0,
-                                },
-                            }}
-                        >
-                            {CardItem(CloudOutlinedIcon)}
-                        </Card>
-                    </NavLink>
-                    <NavLink to={'/test'}>
-                        <Card
-                            sx={{
-                                backgroundColor: 'secondary.dark',
-                                m: 2,
+                                } }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    Histogramme utilisateurs RH
+                                </Typography>
+                                <ChartContainer
+                                    series={series}
+                                    width={500}
+                                    height={400}
+                                    xAxis={[
+                                        {
+                                        id: 'month',
+                                        data: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet"],
+                                        scaleType: 'band',
+                                        valueFormatter: (value) => value.toString(),
+                                        },
+                                    ]}
+                                    yAxis={[
+                                        {
+                                        id: 'eco',
+                                        scaleType: 'linear',
+                                        },
+                                        {
+                                        id: 'pib',
+                                        scaleType: 'log',
+                                        },
+                                    ]}
+                                    >
+                                    <BarPlot />
+                                    <LinePlot />
+                                    <ChartsXAxis label="Mois" position="bottom" axisId="month" />
+                                    <ChartsYAxis label="Nombre" position="left" axisId="eco" />
+                                </ChartContainer>
+                            </CardContent>
+                        </Box>
+                    </Card>
+                    <Card sx={{ display: 'flex', width: 500, m: 2,
                                 [theme.breakpoints.up('lg')]: {
                                     m: 4,
                                 },
@@ -121,12 +197,43 @@ function HomeScreen() {
                                     width: '100%',
                                     my: 1,
                                     mx: 0,
-                                },
-                            }}
-                        >
-                            {CardItem(ShoppingBagOutlinedIcon)}
-                        </Card>
-                    </NavLink>
+                                } }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    Histogramme formations et test
+                                </Typography>
+                                <ChartContainer
+                                    series={seriesformation}
+                                    width={500}
+                                    height={400}
+                                    xAxis={[
+                                        {
+                                        id: 'month',
+                                        data: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet"],
+                                        scaleType: 'band',
+                                        valueFormatter: (value) => value.toString(),
+                                        },
+                                    ]}
+                                    yAxis={[
+                                        {
+                                        id: 'eco',
+                                        scaleType: 'linear',
+                                        },
+                                        {
+                                        id: 'pib',
+                                        scaleType: 'log',
+                                        },
+                                    ]}
+                                    >
+                                    <BarPlot />
+                                    <LinePlot />
+                                    <ChartsXAxis label="Mois" position="bottom" axisId="month" />
+                                    <ChartsYAxis label="Nombre" position="left" axisId="eco" />
+                                </ChartContainer>
+                            </CardContent>
+                        </Box>
+                    </Card>
                 </Box>
             </Box>
         </Fragment>
