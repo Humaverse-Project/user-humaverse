30431c7eba1fa452981550886c2303284c620e1f
SergioDev22 <louis.sergio.rakotoarisoa@esti.mg>
2023-09-27 15:57:57 +0300

    feat :  micmaker le code de grand maitre Félin

--- src/features/metier/components/NewCompetanceModal.jsx
+++ src/features/metier/components/NewCompetanceModal.jsx
@@ -1,104 +1,81 @@
-import React, {useState } from 'react';
+import React, { useState } from "react";
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    Stack,
-    TextField,
-    Typography,
-    Grid,
-    Autocomplete
-} from '@mui/material';
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+  Grid,
+  Autocomplete,
+} from "@mui/material";
 import PartCompetanceCreation from "./partie/PartCompetanceCreation";
-const CreateNewCompetanceModal = ({ open, onClose, onSubmit, rome, competance, appelationlist }) => {
-    const [newmetier, setNewnode] = useState({});
-    const handleSubmit = () => {
-      let data = [];
-      if ("SAVOIRS FAIRE" in competance) {
-        data = data.concat(competance["SAVOIRS FAIRE"])
-      }
-      if ("SAVOIRS" in competance) {
-        data = data.concat(competance["SAVOIRS"])
-      }
-      if ("SAVOIR ÊTRE" in competance) {
-        data = data.concat(competance["SAVOIR ÊTRE"])
-      }
-      console.log(data,newmetier)
-      onSubmit(newmetier, data );
-      onClose();
-    };
-    const handleSliderChange = (newValue, accessitem)=>{
-      accessitem.niveau = newValue
+const CreateNewCompetanceModal = ({
+  open,
+  onClose,
+  onSubmit,
+  rome,
+  competance,
+  appelationlist,
+}) => {
+  const [newmetier, setNewnode] = useState({});
+
+  // Change Value
+  const [value, setValue] = useState(0);
+  const [isNumber, setIsNumber] = useState(false);
+
+  const changeNumber = (e) => {
+    setValue(Number(e.target.value));
+  };
+
+  const handleSubmit = () => {
+    let data = [];
+    if ("SAVOIRS FAIRE" in competance) {
+      data = data.concat(competance["SAVOIRS FAIRE"]);
     }
-    const changetexarea = (e)=>{
-      setNewnode({ ...newmetier, [e.target.name]: e.target.value })
+    if ("SAVOIRS" in competance) {
+      data = data.concat(competance["SAVOIRS"]);
     }
-    return (
-      <Dialog open={open} maxWidth={'md'}>
-        <DialogTitle textAlign="center">Créer une fiche de compétences à partir d'une fiche ROME <b>{rome.code}</b></DialogTitle>
-        <DialogContent  dividers={true}>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <Stack
-              sx={{
-                minWidth: { xs: '300px', sm: '360px', md: '400px' }
-              }}
-            >
-              <Typography variant='h6'> <b>Définition niveau</b></Typography>
-              { "SAVOIRS FAIRE" in competance ? (
-                <><Typography variant='h5' sx={{ mt:2,ml:2 }}>Savoir-faire</Typography>
-                {
-                  competance["SAVOIRS FAIRE"].map((accessitem) => (
-                    < PartCompetanceCreation
-                      handleSliderChange={handleSliderChange}
-                      accessitem= {accessitem}
-                    />
-                  ))
-                }
-                </>
-              ):
-              (null)
-              }
-              { "SAVOIRS" in competance ? (
-                <><Typography variant='h5' sx={{ mt:2,ml:2 }}>Savoirs</Typography>
-                  {
-                    competance["SAVOIRS"].map((accessitem) => (
-                      < PartCompetanceCreation
-                        handleSliderChange={handleSliderChange}
-                        accessitem= {accessitem}
-                      />
-                    ))
-                  }
-                </>
-              ):
-              (null)
-              }
-              { "SAVOIR ÊTRE" in competance ? (
-                <><Typography variant='h5' sx={{ mt:2,ml:2 }}>Savoirs être</Typography>
-                  {
-                    competance["SAVOIR ÊTRE"].map((accessitem) => (
-                      < PartCompetanceCreation
-                        handleSliderChange={handleSliderChange}
-                        accessitem= {accessitem}
-                      />
-                    ))
-                  }
-                </>
-              ):
-              (null)
-              }
-              <Typography sx={{ mt:2,ml:2 }} variant='h6'><b>Proprieté competance</b></Typography>
-              <Grid
-                item
-                xs={12}
-                sm={12}
-                sx={{
-                    display: 'flex',
-                }}
-              >
-                
-                {/* <TextField
+    if ("SAVOIR ÊTRE" in competance) {
+      data = data.concat(competance["SAVOIR ÊTRE"]);
+    }
+    console.log(data, newmetier);
+    setValue(0);
+    onSubmit(newmetier, data);
+    onClose();
+  };
+  const handleSliderChange = (newValue, accessitem) => {
+    accessitem.niveau = newValue;
+  };
+  const changetexarea = (e) => {
+    setNewnode({ ...newmetier, [e.target.name]: e.target.value });
+  };
+  return (
+    <Dialog open={open} maxWidth={"md"}>
+      <DialogTitle textAlign="center">
+        Créer une fiche de compétences à partir d'une fiche ROME{" "}
+        <b>{rome.code}</b>
+      </DialogTitle>
+      <DialogContent dividers={true}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Typography variant="h6">
+            {" "}
+            <b>Appelation</b>
+          </Typography>
+          {/* <Typography sx={{ mt: 2, ml: 2 }} variant="h6">
+          <b>Proprieté competance</b>
+        </Typography> */}
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            sx={{
+              display: "flex",
+            }}
+          >
+            {/* <TextField
                   key="titre"
                   label="Titre"
                   name="titre"
@@ -109,28 +86,40 @@ const CreateNewCompetanceModal = ({ open, onClose, onSubmit, rome, competance, a
                       width: '100%',
                   }}
                 /> */}
-                <Autocomplete
-                    sx={{
-                        m: 2,
-                        width: '100%',
-                    }}
-                    disablePortal
-                    options={appelationlist}
-                    onChange={(e, value) =>{
-                      if (value != null) setNewnode({ ...newmetier, emploistitre: value.label, emploisid: value.id, titre: value.label })
-                    }}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            required
-                            label="Appelation emplois" 
-                            name="apemp"
-                            variant="outlined"
-                            onChange={changetexarea}
-                        />
-                    )}
+            <Autocomplete
+              sx={{
+                m: 2,
+                width: "100%",
+              }}
+              disablePortal
+              options={appelationlist}
+              onChange={(e, value) => {
+                if (value != null) {
+                  setNewnode({
+                    ...newmetier,
+                    emploistitre: value.label,
+                    emploisid: value.id,
+                    titre: value.label,
+                  });
+
+                  setIsNumber(true);
+                } else {
+                  setValue(0);
+                  setIsNumber(false);
+                }
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  label="Appelation emplois"
+                  name="apemp"
+                  variant="outlined"
+                  onChange={changetexarea}
                 />
-                {/* <TextField
+              )}
+            />
+            {/* <TextField
                   key="version"
                   label="Version"
                   name="version"
@@ -142,29 +131,156 @@ const CreateNewCompetanceModal = ({ open, onClose, onSubmit, rome, competance, a
                       width: '100%',
                   }}
                 /> */}
-                <TextField
+            {isNumber && (
+              <TextField
+                required
+                type="number"
+                label="Valeur"
+                name="accretitre"
+                variant="outlined"
+                onChange={changeNumber}
+                sx={{
+                  m: 2,
+                  width: "100%",
+                }}
+              />
+            )}
+          </Grid>
+          <Stack
+            sx={{
+              minWidth: { xs: "300px", sm: "360px", md: "400px" },
+            }}
+          >
+            <Typography variant="h6">
+              {" "}
+              <b>Définition niveau</b>
+            </Typography>
+            {"SAVOIRS FAIRE" in competance ? (
+              <>
+                <Typography variant="h5" sx={{ mt: 2, ml: 2 }}>
+                  Savoir-faire
+                </Typography>
+                {competance["SAVOIRS FAIRE"].map((accessitem) => (
+                  <PartCompetanceCreation
+                    handleSliderChange={handleSliderChange}
+                    accessitem={accessitem}
+                    value={value}
+                  />
+                ))}
+              </>
+            ) : null}
+            {"SAVOIRS" in competance ? (
+              <>
+                <Typography variant="h5" sx={{ mt: 2, ml: 2 }}>
+                  Savoirs
+                </Typography>
+                {competance["SAVOIRS"].map((accessitem) => (
+                  <PartCompetanceCreation
+                    handleSliderChange={handleSliderChange}
+                    accessitem={accessitem}
+                    value={value}
+                  />
+                ))}
+              </>
+            ) : null}
+            {"SAVOIR ÊTRE" in competance ? (
+              <>
+                <Typography variant="h5" sx={{ mt: 2, ml: 2 }}>
+                  Savoirs être
+                </Typography>
+                {competance["SAVOIR ÊTRE"].map((accessitem) => (
+                  <PartCompetanceCreation
+                    handleSliderChange={handleSliderChange}
+                    accessitem={accessitem}
+                    value={value}
+                  />
+                ))}
+              </>
+            ) : null}
+            <Typography sx={{ mt: 2, ml: 2 }} variant="h6">
+              <b>Accréditation</b>
+            </Typography>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              sx={{
+                display: "flex",
+              }}
+            >
+              {/* <TextField
+                  key="titre"
+                  label="Titre"
+                  name="titre"
+                  onChange={changetexarea}
                   required
-                  label="Accredition" 
-                  name="accretitre"
-                  variant="outlined"
+                  sx={{
+                      m: 2,
+                      width: '100%',
+                  }}
+                /> */}
+              {/* <Autocomplete
+                sx={{
+                  m: 2,
+                  width: "100%",
+                }}
+                disablePortal
+                options={appelationlist}
+                onChange={(e, value) => {
+                  if (value != null)
+                    setNewnode({
+                      ...newmetier,
+                      emploistitre: value.label,
+                      emploisid: value.id,
+                      titre: value.label,
+                    });
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    required
+                    label="Appelation emplois"
+                    name="apemp"
+                    variant="outlined"
+                    onChange={changetexarea}
+                  />
+                )}
+              /> */}
+              {/* <TextField
+                  key="version"
+                  label="Version"
+                  name="version"
+                  type='number'
+                  required
                   onChange={changetexarea}
                   sx={{
-                    m: 2,
-                    width: '100%',
+                      m: 2,
+                      width: '100%',
                   }}
-                />
-              </Grid>
-            </Stack>
-          </form>
-        </DialogContent>
-        <DialogActions sx={{ p: '1.25rem' }}>
-          <Button onClick={onClose}>Annuler</Button>
-          <Button color="success" onClick={handleSubmit} variant="contained">
-            Crée le competance
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
+                /> */}
+              <TextField
+                required
+                label="Accredition"
+                name="accretitre"
+                variant="outlined"
+                onChange={changetexarea}
+                sx={{
+                  m: 2,
+                  width: "100%",
+                }}
+              />
+            </Grid>
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: "1.25rem" }}>
+        <Button onClick={onClose}>Annuler</Button>
+        <Button color="success" onClick={handleSubmit} variant="contained">
+          Crée le compétence
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
-export default CreateNewCompetanceModal
+export default CreateNewCompetanceModal;
--- src/features/metier/components/partie/PartCompetanceCreation.jsx
+++ src/features/metier/components/partie/PartCompetanceCreation.jsx
@@ -1,74 +1,96 @@
-import * as React from 'react';
-import { Divider, Grid, ListItem, Slider } from "@mui/material"
-import MuiInput from '@mui/material/Input';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import { Divider, Grid, ListItem, Slider } from "@mui/material";
+import MuiInput from "@mui/material/Input";
+import { styled } from "@mui/material/styles";
 const Input = styled(MuiInput)`
   width: 42px;
 `;
-const PartCompetanceCreation = ({ handleSliderChange, accessitem }) => {
-    const [value, setValue] = React.useState(0);
+const PartCompetanceCreation = ({ handleSliderChange, accessitem, value }) => {
+  const [valueInterne, setValueInterne] = React.useState(0);
 
-    const handleSliderChangeinterne = (newValue, accessitem) => {
-        setValue(newValue);
-        handleSliderChange(newValue, accessitem)
-    };
+  const handleSliderChangeinterne = (newValue, accessitem) => {
+    handleSliderChange(newValue, accessitem);
+  };
 
-    const handleInputChange = (event, accessitem) => {
-        setValue(event.target.value === '' ? 0 : Number(event.target.value));
-        handleSliderChange(event.target.value === '' ? 0 : Number(event.target.value), accessitem)
-    };
+  const handleInputChange = (event, accessitem) => {
+    setValueInterne(Number(event.target.value));
+    handleSliderChange(
+      event.target.value === "" ? 0 : Number(event.target.value),
+      accessitem
+    );
+  };
 
-    const handleBlur = (accessitem) => {
-        if (value < 0) {
-            setValue(0);
-            handleSliderChange(0, accessitem)
-        } else if (value > 100) {
-            setValue(100);
-            handleSliderChange(100, accessitem)
-        }
-    };
-    return (
-        <>
-            <Grid sx={{ flexGrow: 1, mt: 1, pl: 3 }} container spacing={2} key={accessitem.id+"2"}>
-                <Grid item xs={9} key={accessitem.id}>
-                    <ListItem sx={{ p: 0 }} key={accessitem.id+"544"}> {accessitem.brqCompTitre}</ListItem>
-                </Grid>
-                <Grid item xs={3} key={accessitem.id+"range"}>
-                    <Grid container spacing={2} alignItems="center" key={accessitem.id+"container"}>
-                        <Grid item xs={9} key={accessitem.id+"gridslide"}>
-                            <Slider
-                                key={accessitem.id+"slide"}
-                                defaultValue={0}
-                                value={typeof value === 'number' ? value : 0}
-                                onChange={(event, newValue)=>handleSliderChangeinterne(newValue, accessitem)}
-                                valueLabelDisplay="auto"
-                                step={1}
-                                min={0}
-                                max={100}
-                            />
-                        </Grid>
-                        <Grid item xs={3} key={accessitem.id+"gridinput"}>
-                            <Input
-                                key={accessitem.id+"input"}
-                                value={value}
-                                size="small"
-                                onChange={(e)=> handleInputChange(e,accessitem )}
-                                onBlur={()=> handleBlur(accessitem)}
-                                inputProps={{
-                                    step: 10,
-                                    min: 0,
-                                    max: 100,
-                                    type: 'number',
-                                    'aria-labelledby': 'input-slider',
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
-                </Grid>
+  React.useEffect(() => {
+    setValueInterne(value);
+    handleSliderChange(value, accessitem);
+  }, [value]);
+
+  const handleBlur = (accessitem) => {
+    if (value < 0) {
+      setValueInterne(0);
+      handleSliderChange(0, accessitem);
+    } else if (value > 100) {
+      setValueInterne(100);
+      handleSliderChange(100, accessitem);
+    }
+  };
+  return (
+    <>
+      <Grid
+        sx={{ flexGrow: 1, mt: 1, pl: 3 }}
+        container
+        spacing={2}
+        key={accessitem.id + "2"}
+      >
+        <Grid item xs={9} key={accessitem.id}>
+          <ListItem sx={{ p: 0 }} key={accessitem.id + "544"}>
+            {" "}
+            {accessitem.brqCompTitre}
+          </ListItem>
+        </Grid>
+        <Grid item xs={3} key={accessitem.id + "range"}>
+          <Grid
+            container
+            spacing={2}
+            alignItems="center"
+            key={accessitem.id + "container"}
+          >
+            <Grid item xs={9} key={accessitem.id + "gridslide"}>
+              <Slider
+                key={accessitem.id + "slide"}
+                defaultValue={0}
+                value={valueInterne}
+                onChange={(event, newValue) =>
+                  handleSliderChangeinterne(newValue, accessitem)
+                }
+                valueLabelDisplay="auto"
+                step={1}
+                min={0}
+                max={100}
+              />
             </Grid>
-            <Divider variant="middle" />
-        </>
-    )
-}
+            <Grid item xs={3} key={accessitem.id + "gridinput"}>
+              <Input
+                key={accessitem.id + "input"}
+                value={valueInterne}
+                size="small"
+                onChange={(e) => handleInputChange(e, accessitem)}
+                onBlur={() => handleBlur(accessitem)}
+                inputProps={{
+                  step: 10,
+                  min: 0,
+                  max: 100,
+                  type: "number",
+                  "aria-labelledby": "input-slider",
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider variant="middle" />
+    </>
+  );
+};
 
-export default PartCompetanceCreation
+export default PartCompetanceCreation;
